5bb9bc77a55e6943b290743c22e80339c6c8e430
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-11-07 00:43:05 +0000

    rename files to avoid override

--- c/include/file_descriptor.h
+++ c/include/file_descriptor.h
@@ -5,26 +5,26 @@
 #include <stdlib.h>
 
 typedef struct file_handle {
-	struct cfs_node *node;
-	int flags;
-	int in_use;
+  struct cfs_node *node;
+  int flags;
+  int in_use;
 } file_handle;
 
 typedef struct fd_table {
-	size_t handles_cap;
-	file_handle *handles;
+  size_t handles_cap;
+  file_handle *handles;
 } fd_table;
 
 fd_table new_empty(void);
 
 // We have opened a file, create a new File Handle, and add it to the
 // list.
-uint64_t open_node_file(struct fd_table*, struct cfs_node *, int);
+uint64_t open_node_file(struct fd_table *, struct cfs_node *, int);
 
 // Get file handle by index, if out of bounds, NULL will be returned.
-file_handle *get(struct fd_table*, uint64_t id);
+file_handle *get_by_id(struct fd_table *, uint64_t id);
 
 // A file has been closed
-void close(struct fd_table*, uint64_t id);
+void close_by_id(struct fd_table *, uint64_t id);
 
 #endif
